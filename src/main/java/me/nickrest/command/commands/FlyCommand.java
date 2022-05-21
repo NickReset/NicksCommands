2e173a4402ffec0e6e6c263eb9b5a08614331090
NickRest <nickrestemail@gmail.com>
2022-05-21 18:37:12 -0400

    Added /fly <player>

--- src/main/java/me/nickrest/command/commands/FlyCommand.java
+++ src/main/java/me/nickrest/command/commands/FlyCommand.java
@@ -2,6 +2,7 @@ package me.nickrest.command.commands;
 
 import me.nickrest.command.Command;
 import me.nickrest.command.info.CommandInfo;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -11,8 +12,19 @@ import org.jetbrains.annotations.NotNull;
 public class FlyCommand extends Command {
 
     public boolean execute(Player player, String label, String[] args) {
-        player.setAllowFlight(!player.getAllowFlight());
-        player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7You have " + (player.getAllowFlight() ? "&aenabled" : "&cdisabled") + "&r flight!"));
+        if(args.length == 0) {
+            player.setAllowFlight(!player.getAllowFlight());
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&7You have " + (player.getAllowFlight() ? "&aenabled" : "&cdisabled") + "&r flight!"));
+            return false;
+        }
+        Player argPlayer = Bukkit.getPlayer(args[0]);
+        if(argPlayer == null) {
+            player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cPlayer not found!"));
+            return false;
+        }
+        argPlayer.setAllowFlight(!argPlayer.getAllowFlight());
+        player.sendMessage(ChatColor.translateAlternateColorCodes("&", "&7You have " + (argPlayer.getAllowFlight() ? "&aenabled" : "&cdisabled") + "&r flight for &7" + argPlayer.getName() + "&r!"));
+
         return false;
     }
 }
