3acf71bf089667b1afcc4a346fce36ce9be04b07
NickRest <nickrestemail@gmail.com>
2022-05-21 18:31:23 -0400

    Updated plugin.yml

--- src/main/java/me/nickrest/Plugin.java
+++ src/main/java/me/nickrest/Plugin.java
@@ -1,16 +1,23 @@
 package me.nickrest;
 
 import lombok.Getter;
+import lombok.Setter;
+import me.nickrest.command.manager.CommandManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+@Getter @Setter
 public final class Plugin extends JavaPlugin {
 
     /* instance */
     @Getter
     private static Plugin instance;
 
+    /* managers */
+    private CommandManager commandManager;
+
     public void onEnable() {
         instance = this;
+        commandManager = new CommandManager();
     }
 
 }
