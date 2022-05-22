1dcd12c6c65f86a1487ac45286bff7a20f1ca122
NickRest <nickrestemail@gmail.com>
2022-05-22 13:19:00 -0400

    Created setNameTag()

--- src/main/java/me/nickrest/util/NameTagEdit.java
+++ src/main/java/me/nickrest/util/NameTagEdit.java
@@ -1,7 +1,24 @@
 package me.nickrest.util;
 
 import lombok.experimental.UtilityClass;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Team;
 
 @UtilityClass
 public class NameTagEdit {
+
+    public static void setNameTag(Player player, String name) {
+        Team team = player.getScoreboard().getTeam(player.getName());
+        if (team == null) {
+            team = player.getScoreboard().registerNewTeam(player.getName());
+        }
+        team.setPrefix(name);
+        team.setDisplayName(name);
+        team.setAllowFriendlyFire(false);
+        team.setCanSeeFriendlyInvisibles(true);
+        team.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.ALWAYS);
+        team.setOption(Team.Option.COLLISION_RULE, Team.OptionStatus.NEVER);
+        team.setOption(Team.Option.DEATH_MESSAGE_VISIBILITY, Team.OptionStatus.NEVER);
+        team.setOption(Team.Option.NAME_TAG_VISIBILITY, Team.OptionStatus.ALWAYS);
+    }
 }
